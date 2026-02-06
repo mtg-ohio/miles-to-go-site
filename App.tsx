7cff766bd6a792b2885a70e375bd2b29917d668c
mtg-ohio <michael@milestogoohio.com>
2026-02-05 22:55:39 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import ProblemSection from './components/ProblemSection';
+import BaselineSection from './components/BaselineSection';
+import DifferenceSection from './components/DifferenceSection';
+import RoadmapSection from './components/RoadmapSection';
+import BattlefieldSection from './components/BattlefieldSection';
+import PricingSection from './components/PricingSection';
+import ScaleSection from './components/ScaleSection';
+import Footer from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-white font-sans text-mtg-text">
+      <Header />
+      <main>
+        <Hero />
+        <ProblemSection />
+        <BaselineSection />
+        <DifferenceSection />
+        <RoadmapSection />
+        <BattlefieldSection />
+        <PricingSection />
+        <ScaleSection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
